0726afb43ccdad931f2d4867aeaecff1015d7ea4
Matt Hillsdon <matt.hillsdon@microbit.org>
2022-08-01 16:29:48 +0100

    Fix sensor validation.
    As well as not being very careful, the previous implementation ignored
    zero values for sensors.
    
    Move the validation into the sensor objects which can more easily check
    the expected type.

--- src/board/sensors.ts
+++ src/board/sensors.ts
@@ -1,5 +1,11 @@
 export class Sensor {
   constructor(public type: string, public id: string) {}
+
+  protected valueError(value: any) {
+    return new Error(
+      `${this.id} given invalid value: ${JSON.stringify(value)}`
+    );
+  }
 }
 
 export class RangeSensor extends Sensor {
@@ -14,6 +20,25 @@ export class RangeSensor extends Sensor {
     super("range", id);
     this.value = initial;
   }
+
+  setValue(value: any): void {
+    let proposed: number;
+    if (typeof value === "number") {
+      proposed = value;
+    } else if (typeof value === "string") {
+      try {
+        proposed = parseInt(value, 10);
+      } catch (e) {
+        throw this.valueError(value);
+      }
+    } else {
+      throw this.valueError(value);
+    }
+    if (proposed > this.max || proposed < this.min) {
+      throw this.valueError(value);
+    }
+    this.value = proposed;
+  }
 }
 
 export class EnumSensor extends Sensor {
@@ -23,5 +48,12 @@ export class EnumSensor extends Sensor {
     this.id = id;
     this.choices = choices;
     this.value = initial;
+  }
+
+  setValue(value: any): void {
+    if (typeof value !== "string" || !this.choices.includes(value)) {
+      throw this.valueError(value);
+    }
+    this.value = value;
   }
 }

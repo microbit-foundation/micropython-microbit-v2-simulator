a5be6105ac019401b5fd271db0d71225e2c9757c
Matt Hillsdon <matt.hillsdon@microbit.org>
2022-11-15 12:29:15 +0000

    Install a fixed build of emscripten.
    We've not seen issues using latest but it's likely to break us
    eventually.
    
    Closes https://github.com/microbit-foundation/micropython-microbit-v2-simulator/issues/15

--- ci-build.sh
+++ ci-build.sh
@@ -6,10 +6,11 @@ git submodule update --init lib/micropython-microbit-v2
 (cd lib/micropython-microbit-v2 && git submodule update --init lib/micropython)
 
 # Emscripten
+VERSION="3.1.25"
 export PYTHON=python3.7 # Needed by Emscripten in Netlify's build image.
-git clone https://github.com/emscripten-core/emsdk.git ~/.emsdk
-~/.emsdk/emsdk install latest
-~/.emsdk/emsdk activate latest
+git clone https://github.com/emscripten-core/emsdk.git -b $VERSION ~/.emsdk
+~/.emsdk/emsdk install $VERSION
+~/.emsdk/emsdk activate $VERSION
 source ~/.emsdk/emsdk_env.sh
 
 npm run test && npm run build

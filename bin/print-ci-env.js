fc2093b102f9e062b682d5f42b18261616dfd5d4
Matt Hillsdon <44397098+microbit-matt-hillsdon@users.noreply.github.com>
2023-02-06 16:23:48 +0000

    Migrate to GitHub actions (#98)
    Sticking with Node 14 for now to match Emscripten.

---
+++ bin/print-ci-env.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const ref = process.env.GITHUB_REF;
+let stage;
+if (ref === "refs/heads/main") {
+  stage = "STAGING";
+} else if (ref.startsWith("refs/tags/v")) {
+  stage = "PRODUCTION";
+} else {
+  stage = "REVIEW";
+}
+
+console.log(`STAGE=${stage}`);

292addc6948637b3bfae42767ec78b083f28bb48
Grace <grace@microbit.org>
2024-03-21 14:47:23 +0000

    Update pin_touches sample to include logo

--- src/examples/pin_touches.py
+++ src/examples/pin_touches.py
@@ -1,12 +1,13 @@
 from microbit import *
 
-def report_pin_touches(pin, p_num):
+def report_pin_touches(pin, pin_type):
     if pin.was_touched():
-        print('pin', p_num, 'was touched', pin.get_touches(), 'time(s)')
+        print('pin', pin_type, 'was touched', pin.get_touches(), 'time(s)')
 
 while True:
-    if pin_logo.is_touched():
+    if button_a.is_pressed():
         report_pin_touches(pin0, "0")
         report_pin_touches(pin1, "1")
         report_pin_touches(pin2, "2")
+        report_pin_touches(pin_logo, "logo")
         break

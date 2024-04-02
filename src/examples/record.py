8eb6fa7a424d417a1a87863325426994044fa17f
Matt Hillsdon <matt.hillsdon@microbit.org>
2024-04-02 09:54:13 +0100

    Update

--- src/examples/record.py
+++ src/examples/record.py
@@ -2,6 +2,6 @@ from microbit import microphone, audio
 
 frame = microphone.record(1000)
 print(len(frame))
-for n in range(0, 10):
-  print(frame[n])
+for n in range(0, len(frame)):
+    print(frame[n], end=',')
 audio.play(frame)
